4170b6997622e9531b42dcedb54df9bd7eec50df
CaptainButter8 <tardis241@gmail.com>
2025-09-17 12:59:05 -0600

    Testing code in progress

--- test/test.c
+++ test/test.c
@@ -3,36 +3,66 @@
 #include <stdint.h>
 #include <unity.h>
 #include "unity_config.h"
+#include <pico/multicore.h>
+#include <pico/cyw43_arch.h>
+#include <semphr.h>
+#include <FreeRTOS.h>
+
+
+#include <unity.h>
+#include "unity_config.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <FreeRTOS.h>
+#include <semphr.h>
+#include <task.h>
+#include <pico/stdlib.h>
+#include <pico/multicore.h>
+#include <pico/cyw43_arch.h>
+SemaphoreHandle_t semaphore;
+
+int counter;
+int on;
+
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_variable_assignment()
-{
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+void side_thread(void * params){
+    if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
+        sleep_ms(100); 
+        printf("hello world from %s! Count %d\n", "thread", counter++);
+        xSemaphoreGive(semaphore);
+    }
 }
 
-void test_multiplication(void)
-{
-    int x = 30;
-    int y = 6;
-    int z = x / y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
+void main_thread(void * params){
+    if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+        sleep_ms(100); 
+        printf("hello world from %s! Count %d\n", "main", counter++);
+        on = !on;
+        xSemaphoreGive(semaphore);
+    }
 }
-
 int main (void)
 {
     stdio_init_all();
+    hard_assert(cyw43_arch_init() == PICO_OK);
+    on = false;
+    counter = 0;
+    TaskHandle_t main, side;
+    semaphore = xSemaphoreCreateCounting(1, 1);
     sleep_ms(5000); // Give time for TTY to attach.
-    
-    while(1) {
+    TEST_ASSERT_TRUE_MESSAGE(sempahore != NULL, "Semaphore is returned as NULL");
+    if (semaphore != NULL){
         printf("Start tests\n");
         UNITY_BEGIN();
-        RUN_TEST(test_variable_assignment);
-        RUN_TEST(test_multiplication);
+        side_thread("TestSideThread");
         sleep_ms(5000);
-        UNITY_END();
+        main_thread("TestMainThread");
+        while(1) {sleep_ms(5000);}
+        return UNITY_END();
     }
 }
