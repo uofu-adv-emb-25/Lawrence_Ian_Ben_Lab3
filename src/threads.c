b023b9e688af2a5ed80a99140c63c8be65a096ac
CaptainButter8 <tardis241@gmail.com>
2025-09-17 11:52:18 -0600

    Fixed threads.c to use semaphoretake and semaphoregive.

--- src/threads.c
+++ src/threads.c
@@ -20,19 +20,26 @@ int on;
 void side_thread(void *params)
 {
 	while (1) {
-        vTaskDelay(100);
-        counter += counter + 1;
-		printf("hello world from %s! Count %d\n", "thread", counter);
+        if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
+
+            vTaskDelay(100);
+            printf("hello world from %s! Count %d\n", "thread", counter++);
+            xSemaphoreGive(semaphore);
+        }
 	}
 }
 
 void main_thread(void *params)
 {
 	while (1) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        vTaskDelay(100);
-		printf("hello world from %s! Count %d\n", "main", counter++);
-        on = !on;
+        if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+            vTaskDelay(100);
+		    printf("hello world from %s! Count %d\n", "main", counter++);
+            on = !on;
+            xSemaphoreGive(semaphore);
+        }
+
 	}
 }
 
@@ -44,10 +51,13 @@ int main(void)
     counter = 0;
     TaskHandle_t main, side;
     semaphore = xSemaphoreCreateCounting(1, 1);
-    xTaskCreate(main_thread, "MainThread",
-                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &main);
-    xTaskCreate(side_thread, "SideThread",
-                SIDE_TASK_STACK_SIZE, NULL, SIDE_TASK_PRIORITY, &side);
-    vTaskStartScheduler();
+    if (semaphore != NULL)
+    {
+        xTaskCreate(main_thread, "MainThread",
+                    MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &main);
+        xTaskCreate(side_thread, "SideThread",
+                    SIDE_TASK_STACK_SIZE, NULL, SIDE_TASK_PRIORITY, &side);
+        vTaskStartScheduler();
+    }
 	return 0;
 }
